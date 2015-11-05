46b9781e742a0e61cf9b8da9bd253fa8b991ea28
ArtemSherenkovskiy <superdyatel@gmail.com>
2015-11-05 13:32:09 +0200

    create things list

--- tableresult.cpp
+++ tableresult.cpp
@@ -8,6 +8,7 @@ TableResult::TableResult(QWidget *parent) :
     ui->setupUi(this);
     temperatures = new int[12];
     createTemp();
+    createThings();
 }
 
 TableResult::~TableResult()
@@ -32,4 +33,26 @@ void TableResult::createTemp()
     {
         qDebug() << temperatures[i] << "\t";
     }
+}
+
+void TableResult::createThings()
+{
+    THINGS = new List<Thing>();
+    THINGS.append(new Thing(1, "Blazer", 0.5, 6));
+    THINGS.append(new Thing(2, "Bushlat", 4, 48));
+    THINGS.append(new Thing(3, "Vatnie shtani", 2, 24));
+    THINGS.append(new Thing(4, "V`etnams", 0.5, 6));
+    THINGS.append(new Thing(5, "Jeanse", 1, 12));
+    THINGS.append(new Thing(6, "Kepka", 0.5, 6));
+    THINGS.append(new Thing(7, "Krossi", 1, 12));
+    THINGS.append(new Thing(8, "Kurtka", 2, 24));
+    THINGS.append(new Thing(9, "Palto", 3, 36));
+    THINGS.append(new Thing(10, "Rukavichki", 0.5, 6));
+    THINGS.append(new Thing(11, "Svitr", 1, 12));
+    THINGS.append(new Thing(12, "Rubashka", 0.5, 6));
+    THINGS.append(new Thing(13, "Futbolka", 0.5, 6));
+    THINGS.append(new Thing(14, "Botinki", 1.5, 18));
+    THINGS.append(new Thing(15, "Bashmaki", 2, 24));
+    THINGS.append(new Thing(16, "Shapka", 1, 12));
+    THINGS.append(new Thing(17, "Shorti", 0.5, 6));
 }
